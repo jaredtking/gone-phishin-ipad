875feb3deb17c47972310c591e2617ee86c5b2d8
wolverine <wolverine@SEAT-MAC-MINI-1.local>
2013-11-15 14:23:52 -0600

    added main view controller

---
+++ Gone Phishin'/Gone Phishin'/Classes/GPMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPMainViewController.h
+//  Gone Phishin'
+//
+//  Created by wolverine on 11/15/13.
+//  Copyright (c) 2013 wolverine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPMainViewController : UIViewController
+
+@end
---
+++ Gone Phishin'/Gone Phishin'/Classes/GPMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  GPMainViewController.m
+//  Gone Phishin'
+//
+//  Created by wolverine on 11/15/13.
+//  Copyright (c) 2013 wolverine. All rights reserved.
+//
+
+#import "GPMainViewController.h"
+
+@interface GPMainViewController ()
+
+@end
+
+@implementation GPMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
